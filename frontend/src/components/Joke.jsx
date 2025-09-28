871c21f891601fc4e31f639740cf3afe4c7be64a
bviktoria <barcsaviktoria@mailbox.unideb.hu>
2025-09-29 00:25:48 +0200

    feat: implemented joke fetch upon click

--- frontend/src/components/Joke.jsx
+++ frontend/src/components/Joke.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Joke() {
+
+    const [joke, setJoke] = useState(false);
+
+    const fetchJokes = async () => {
+        fetch("https://api.chucknorris.io/jokes/random")
+            .then(response => response.json())
+            .then(joke => setJoke(joke.value))
+    }
+
     return (
         <>
             <div className="flex items-center justify-center h-screen">
                 <div className="bg-transparent text-white p-20 rounded shadow-2xl break-all">
-                    <p className="text-black ">"When Chuck Norris asks to 'borrow' something from you, be aware that you will never, ever get it back and are in fact about to be kicked in the face.</p>
-                    <button className="block hover:bg-blue-300 text-black  text-lg mx-auto p-4 rounded-4xl cursor-pointer">Generate a joke</button>
-
+                    <p className="text-black ">{joke}</p>
+                    <button onClick={fetchJokes} className="block hover:bg-blue-300 text-black  text-lg mx-auto p-4 rounded-4xl cursor-pointer">Generate a joke</button>
                 </div>
             </div>
-
         </>
     )
 }
